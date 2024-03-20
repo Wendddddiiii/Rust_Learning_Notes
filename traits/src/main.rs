27626abe51cf84bbb24d0066c699b919363ce8c6
Wendddddiiii <wenditang058@gmail.com>
2024-03-21 01:10:03 +1100

    object safe

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -245,49 +245,142 @@
 
 
 //array with Trait Objects
-trait Bird {
-    fn quack(&self);
+// trait Bird {
+//     fn quack(&self);
+// }
+
+// struct Duck;
+// impl Duck {
+//     fn fly(&self) {
+//         println!("Look, the duck is flying")
+//     }
+// }
+
+// struct Swan;
+// impl Swan {
+//     fn fly(&self) {
+//         println!("Look, the duck, oh, sorry, the swan is flying")
+//     }
+// }
+
+// impl Bird for Duck {
+//     fn quack(&self) {
+//         println!("{}", "duck duck");
+//     }
+// }
+
+// impl Bird for Swan {
+//     fn quack(&self) {
+//         println!("{}", "swan swan");
+//     }
+// }
+
+// fn main() {
+//     let birds: [&dyn Bird; 2] = [&Duck, &Swan]; //usize
+//     for bird in birds {
+//         bird.quack(); 
+//     }
+
+//     println!("success!");
+// }
+
+// fn hatch_a_bird(species: u8) -> Box<dyn Bird> {
+//     match species {
+//         1 => Box::new(Swan),
+//         2 => Box::new(Duck),
+//         _ => panic!(),
+//     }
+// }
+
+
+// //&dyn and Box<&dyn>
+// trait Draw {
+//     fn draw(&self) -> String;
+// }
+
+// impl Draw for u8 {
+//     fn draw(&self) -> String {
+//         format!("u8: {}", self)
+//     }
+// }
+// impl Draw for f64 {
+//     fn draw(&self) -> String {
+//         format!("f64:{}", self)
+//     }
+// }
+
+// fn main() {
+//     let x: f64 = 1.19f64;
+//     let y: u8 = 8u8;
+
+
+//     //draw x
+//     draw_with_box(Box::new(x));
+
+//     //draw y 
+//     draw_with_ref(&y);
+
+//     println!("success!");
+// }
+
+// fn draw_with_box(x: Box<dyn Draw>) {
+//     x.draw();
+// }
+
+// fn draw_with_ref(x: &dyn Draw) {
+//     x.draw();
+// }
+
+
+// trait Foo {
+//     fn method(&self) -> String;
+// }
+
+// impl Foo for u8 {
+//     fn method(&self) -> String {format!("u8: {}", *self)}
+// }
+
+// impl Foo for String {
+//     fn method(&self) -> String {format!("string: {}", *self)}
+// }
+
+// fn static_dispatch<T: Foo>(a: T) {
+//     a.method();
+// }
+
+// fn dynamic_dispatch(a: &dyn Foo) {
+//     a.method();
+// }   
+
+// fn main() {
+//     let x: u8 = 5u8;
+//     let y: String = "hello".to_string();
+//     static_dispatch(x);
+//     dynamic_dispatch(&y);
+
+//     println("success!");
+// }
+
+
+//object Safe
+trait MyTrait {
+    fn f(&self) -> Box<dyn MyTrait>;
 }
 
-struct Duck;
-impl Duck {
-    fn fly(&self) {
-        println!("Look, the duck is flying")
-    }
+impl MyTrait for u32 {
+    fn f(&self) -> Box<dyn MyTrait> {Box::new(42)}
 }
 
-struct Swan;
-impl Swan {
-    fn fly(&self) {
-        println!("Look, the duck, oh, sorry, the swan is flying")
-    }
+impl MyTrait for String {
+    fn f(&self) -> Box<dyn MyTrait> {Box::new(self.clone())}
 }
 
-impl Bird for Duck {
-    fn quack(&self) {
-        println!("{}", "duck duck");
-    }
-}
-
-impl Bird for Swan {
-    fn quack(&self) {
-        println!("{}", "swan swan");
-    }
+fn my_function(x: Box<dyn MyTrait>) -> Box<dyn MyTrait> {
+    x.f()
 }
 
 fn main() {
-    let birds: [&dyn Bird; 2] = [&Duck, &Swan]; //usize
-    for bird in birds {
-        bird.quack(); 
-    }
-
+    my_function(Box::new(13_u32));
+    my_function(Box::new(String::from("abc")));
     println!("success!");
-}
-
-fn hatch_a_bird(species: u8) -> Box<dyn Bird> {
-    match species {
-        1 => Box::new(Swan),
-        2 => Box::new(Duck),
-        _ => panic!(),
-    }
 }
